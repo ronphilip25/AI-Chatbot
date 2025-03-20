c511567eb8b91d9bbd6bd23c10d729a5c4a8c583
ronlip2571 <sanchezronphilip@gmail.com>
2025-03-20 23:12:16 +0800

    change the redirect url

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -10,7 +10,7 @@ export default function Login() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}`,
       },
     });
 
