424664a642e5bae334019aff35d36134bd3a779f
Ron Philip <sanchezronphilip@gmail.com>
2025-03-21 08:51:21 +0800

    added a cursor pointer

--- src/app/logged-out/page.jsx
+++ src/app/logged-out/page.jsx
@@ -18,7 +18,7 @@ export default function LoggedOut() {
       <h1 className="text-3xl font-bold">❌ You are logged out!</h1>
       <p className="text-lg mt-2">Redirecting to home...</p>
       <button
-        className="mt-5 bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded"
+        className="mt-5 cursor-pointer bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded"
         onClick={() => router.push("/")}
       >
         Go Home Now
